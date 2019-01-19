73ac9f5afc8b615d92d884e1ef08565c70a262b4
SeizeThed4y <frk.coskunn@gmail.com>
2019-01-19 14:33:37 -0500

    Version 4 with Multiply Method

--- src/CalculatorJava.java
+++ src/CalculatorJava.java
@@ -9,4 +9,9 @@ public class CalculatorJava {
     public static double substracts(double num1 , double num2){
         return num1 - num2;
     }
+
+
+    public static double multiply(double num1, double num2){
+        return num1 * num2;
+    }
 }
