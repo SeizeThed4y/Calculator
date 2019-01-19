17b0685637496d8004c7570d87f30e52b196f93c
SeizeThed4y <frk.coskunn@gmail.com>
2019-01-19 12:43:41 -0500

    add the method

--- src/CalculatorJava.java
+++ src/CalculatorJava.java
@@ -4,4 +4,9 @@ public class CalculatorJava {
     public static double add(double num1, double num2){
         return num1 + num2;
     }
+
+
+    public static double subtract(double num1 , double num2){
+        return num1 - num2;
+    }
 }
